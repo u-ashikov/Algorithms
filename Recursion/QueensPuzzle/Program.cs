5531a84bb2ff2c6981a0eb7561bd6e377644102b
Ulian Ashikov <u.ashikov@abv.bg>
2018-03-23 00:13:00 +0200

    Eight queens puzzle

---
+++ Recursion/QueensPuzzle/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace QueensPuzzle
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        private const int Size = 8;
+
+        private static bool[,] chessboard = new bool[Size, Size];
+
+        private static HashSet<int> attackedRows = new HashSet<int>();
+        private static HashSet<int> attackedCols = new HashSet<int>();
+        private static HashSet<int> attackedLeftDiagonals = new HashSet<int>();
+        private static HashSet<int> attackedRightDiagonals = new HashSet<int>();
+
+        public static void Main()
+        {
+            PutQueens();   
+        }
+
+        private static void PutQueens(int row = 0)
+        {
+            if (row == Size)
+            {
+                PrintSolution();
+            }
+            else
+            {
+                for (int col = 0; col < Size; col++)
+                {
+                    if (CanPlaceQueen(row,col))
+                    {
+                        MarkAllAttackedPositions(row, col);
+                        PutQueens(row + 1);
+                        UnmarkAllAttackedPositions(row, col);
+                    }
+                }
+            }
+        }
+
+        private static void UnmarkAllAttackedPositions(int row, int col)
+        {
+            attackedRows.Remove(row);
+            attackedCols.Remove(col);
+            attackedLeftDiagonals.Remove(col - row);
+            attackedRightDiagonals.Remove(row + col);
+
+            chessboard[row, col] = false;
+        }
+
+        private static void MarkAllAttackedPositions(int row, int col)
+        {
+            attackedRows.Add(row);
+            attackedCols.Add(col);
+            attackedLeftDiagonals.Add(col - row);
+            attackedRightDiagonals.Add(row + col);
+
+            chessboard[row,col] = true;
+        }
+
+        private static bool CanPlaceQueen(int row, int col)
+        {
+            var occupiedPositions =  attackedRows.Contains(row) ||
+                attackedCols.Contains(col) ||
+                attackedLeftDiagonals.Contains(col - row) ||
+                attackedRightDiagonals.Contains(row + col);
+
+            return !occupiedPositions;
+        }
+
+        private static void PrintSolution()
+        {
+            for (int row = 0; row < Size; row++)
+            {
+                for (int col = 0; col < Size; col++)
+                {
+                    if(chessboard[row,col])
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
